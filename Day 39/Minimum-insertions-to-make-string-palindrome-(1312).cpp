013963829c2fc5058fccb410d4f0f7e088519652
Divanshu Rawat <divanshurawat64@gmail.com>
2024-08-23 23:34:30 +0530

    Day 39 done

---
+++ Day 39/Minimum-insertions-to-make-string-palindrome-(1312).cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int n;
+    int dp[501][501];
+    int solve(int i, int j, string &s){
+        if(i>=j) return 0;
+        if(dp[i][j]!=-1) return dp[i][j];
+        if(s[i]==s[j]){
+            return  dp[i][j]= solve(i+1, j-1, s);
+        }
+        
+        return dp[i][j]= 1+ min(solve(i+1,j,s), solve(i, j-1,s));
+      
+    }
+    int minInsertions(string s) {
+        memset(dp, -1, sizeof(dp));
+        n=s.size();
+        return solve(0,n-1,s);
+    }
+};
